c3782755e3198d21b12fde348d7cf54f8e8fcd06
Steve Godin <steve.god@gmail.com>
2017-02-03 11:52:01 -0800

    Add bootstrap demo code

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Adding Bootstrap to your Express app
+// Step 1: Make sure it can serve static assets
+// Step 2: Create public/ folder and put Bootstrap assets in it
+// Step 3: Add link & script tag in header partial to load Bootstrap
+// Step 4: Add jQuery library dependency to project
+
 // Node Modules
 const path = require('path');
 // Express
@@ -12,6 +18,9 @@ const home = require('./routes/home');
 const app = Express();
 
 app.set('view engine', 'ejs');
+// The following will serve files from the public folder to
+// the root of our app http://localhost:4545/
+app.use(Express.static(path.join(__dirname, 'public')));
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(logger('dev'))
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,22 @@
 const Express = require('express');
 const router = Express.Router();
+// 👇 library that can generate all sorts of random data
+// https://github.com/marak/Faker.js/
+const faker = require('faker');
 
 router.get('/', function (request, response) {
+  response.render('home/index');
 })
 
+router.get('/about', function (request, response) {
+  response.render('home/about', {faker}); // 👈 equivalent to {faker: faker}
+})
 
+router.get('/contact', function (request, response) {
+  response.render('home/contact');
+})
+
+router.get('/grid-demo', function (request, response) {
+  response.render('home/grid-demo');
+})
 module.exports = router;
